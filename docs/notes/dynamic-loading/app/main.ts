32f588b5bcb841d1db507e03c895b737fb6de735
Mark Rudolph <mark_rudolph@yahoo.com>
2016-06-09 16:35:53 -0700

    cleaned previous mf2 generated docs; updated .gitignore to ignore more tmp files

--- docs/notes/dynamic-loading/app/main.ts
+++
@@ -1,6 +0,0 @@
-
-import { bootstrap } from '@angular/platform-browser-dynamic';
-
-import { StructuralDirectivesComponent } from './structural-directives.component';
-
-bootstrap(StructuralDirectivesComponent);
